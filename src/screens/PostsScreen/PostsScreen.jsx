4a32a398c4a0deddc43929b454dbe1117ba405c0
SolusBlade <kykysha59fafa@gmail.com>
2023-07-16 13:48:29 +0300

    hw3 done

---
+++ src/screens/PostsScreen/PostsScreen.jsx
@@ -0,0 +1,14 @@
+import { Text, View, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import styles from '../../assets/styles/styles';
+
+const PostsScreen = () => {
+    
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default PostsScreen;
